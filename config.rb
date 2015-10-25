0fe5a8a7306055d78f246875605cbb04ead3da08
Viktor Kalinin <kalinin.viktor@gmail.com>
2015-10-25 18:46:44 +0200

    Add basic folder structure + compass config

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'sass-globbing'
+require 'breakpoint'
+
+project_type = :stand_alone
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'assets'
+images_dir = 'img'
+javascripts_dir = 'js'
+fonts_dir = 'fonts'
+output_style = :expanded
